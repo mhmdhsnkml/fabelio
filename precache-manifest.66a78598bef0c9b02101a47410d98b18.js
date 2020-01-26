d88c0d18aa8214c742ed539531463b0d42913c61
Muhamad Ihsan Kamil <ihsan@moodah.id>
2020-01-26 21:51:15 +0700

    Updates

---
+++ precache-manifest.66a78598bef0c9b02101a47410d98b18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a11f3d6402ce92c3e5b9b304663ac97",
+    "url": "/fabelio/index.html"
+  },
+  {
+    "revision": "fe4a4a62dcfe01a860b5",
+    "url": "/fabelio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e3ee95c2c5458cdad12a",
+    "url": "/fabelio/static/css/main.5e11a1e6.chunk.css"
+  },
+  {
+    "revision": "fe4a4a62dcfe01a860b5",
+    "url": "/fabelio/static/js/2.a349b63a.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/fabelio/static/js/2.a349b63a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3ee95c2c5458cdad12a",
+    "url": "/fabelio/static/js/main.9f23ff5f.chunk.js"
+  },
+  {
+    "revision": "770c7910913b892e62b9",
+    "url": "/fabelio/static/js/runtime-main.d69d99b6.js"
+  }
+]);
